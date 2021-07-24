e8f5a2305ef6d457201959e37a39e76c2f31e4f9
kengsini250 <kengsini250@gmail.com>
2021-07-24 14:29:51 +0800

    v1.1.0

---
+++ mainwindow.cpp
@@ -0,0 +1,44 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#define X 350
+#define Y 30
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    avl = new AVL;
+    connect(ui->pushButton,&QPushButton::clicked,this,[this]{
+        if(avl->isEmpty())
+            avl->add(new Node(ui->lineEdit->text().toInt(),X,Y));
+        else
+            avl->add(new Node(ui->lineEdit->text().toInt()));
+
+        update();
+    });
+    //avl->add(new Node(10,X,Y));
+    //avl->add(new Node(8));
+//    avl->add(new Node(12));
+    //avl->add(new Node(6));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::paintEvent(QPaintEvent *)
+{
+    
+    QPainter painter(this);
+    auto arr = avl->getArr();
+    for(auto n=arr.begin();n!=arr.end();n++){
+        painter.drawEllipse((*n)->x,(*n)->y,50,50);
+        painter.drawText((*n)->x + 20,(*n)->y + 27,QString::number((*n)->data));
+    }
+    
+}
+
